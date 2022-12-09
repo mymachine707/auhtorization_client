2a9faee9438d370cda241ece4601c9f571051f54
mymachine707 <mymaxs441@gmail.com>
2022-12-09 09:06:13 +0500

    git commit successfuly

--- storage/migrations/01_init_database_tables.up.sql
+++ storage/migrations/01_init_database_tables.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS client (
 	"id" CHAR(36) PRIMARY KEY,
 	"firstname" VARCHAR(255) NOT NULL,
 	"lastname" VARCHAR(255) NOT NULL,
-   "phone" VARCHAR(255) NOT NULL,
-   "address" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(255) NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
 	"created_at" TIMESTAMP DEFAULT now(),
 	"updated_at" TIMESTAMP,
 	"deleted_at" TIMESTAMP
