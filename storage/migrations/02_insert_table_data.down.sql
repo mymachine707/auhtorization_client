c4dc666c5877b9dbd83b272d370a60d29130641f
mymachine707 <mymaxs441@gmail.com>
2022-12-13 17:49:35 +0500

    Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida

--- storage/migrations/02_insert_table_data.down.sql
+++
@@ -1,8 +0,0 @@
-
-BEGIN;
-
-	DELETE FROM client where id='d7dc9b63-50fe-4c15-8020-e77d274afcc6';
-	DELETE FROM client where id='4587750c-1903-4a26-bb92-c84d61093629';
-	DELETE FROM client where id='62ffaa19-ba46-456d-be7d-e4fece7dd2ce';	
-
-COMMIT;
